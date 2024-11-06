c992e5ed18f83df011764a8915593ecebf835eb3
FranxJunior <franklinjunior021118@gmail.com>
2024-11-06 16:22:39 -0500

    feat: Agregar constante de URL_BACKEND
    Agrega la constante URL_BACKEND con el valor 'http://localhost:3000/' en el archivo consts.ts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,18 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    output:'server',
-    integrations: [mdx(), sitemap(), tailwind({
-        applyBaseStyles: false,
-    }), react()],
+  site: 'https://example.com',
+  output:'server',
+
+  integrations: [mdx(), sitemap(), tailwind({
+      applyBaseStyles: false,
+  }), react()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Biblioteca Virtual';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const URL_BACKEND = 'http://localhost:3000/';
